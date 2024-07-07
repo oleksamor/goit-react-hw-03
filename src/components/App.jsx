27f373373c7c388563f40b3f8e2a951c470f3fed
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-07-07 15:13:23 +0300

    add state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState([
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ]);
 
-  return <></>;
+  return (
+    <div>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
