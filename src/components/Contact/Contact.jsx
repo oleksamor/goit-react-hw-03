983f6a767082838c0a6110da29b52b5e34a11bca
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-07-13 10:23:21 +0300

    upd style contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,15 @@
 import s from "./Contact.module.css";
 
-// import { FaUserAlt } from "react-icons/fa";
-// import { FaPhone } from "react-icons/fa6";
-
 const Contact = ({ name, number, id, deleteContact }) => {
   return (
     <div className={s.wrapper}>
       <div className={s.userWrapper}>
         <p className={s.text}>
-          <span className={s.span}>
-            {/* <FaUserAlt color="rgb(51, 51, 105)" /> */}
-          </span>
+          <span className={s.span}></span>
           {name}
         </p>
         <p className={s.text}>
-          <span className={s.span}>
-            {/* <FaPhone color="rgb(51, 51, 105)" /> */}
-          </span>
+          <span className={s.span}></span>
           {number}
         </p>
       </div>
